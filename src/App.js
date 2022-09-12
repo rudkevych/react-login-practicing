6a5be09e3d635ab654daa7fae88895e960895ae4
rudkevych <oksana.rudkevych@gmail.com>
2022-09-12 22:50:58 +0200

    refac: added auth context instead of passing values

--- src/App.js
+++ src/App.js
@@ -3,38 +3,43 @@ import React, { useState, useEffect } from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './store/auth-context';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLogged, setIsLogged] = useState(false);
 
   useEffect(() => {
-      const isLogged = localStorage.getItem('isLoggedIn');
+    const isLogged = localStorage.getItem('isLoggedIn');
 
-      if(isLogged) {
-        setIsLoggedIn(true);
-      }
+    if (isLogged) {
+      setIsLogged(true);
+    }
   }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
     localStorage.setItem('isLoggedIn', 1);
-    setIsLoggedIn(true);
+    setIsLogged(true);
   };
 
   const logoutHandler = () => {
     localStorage.removeItem('isLoggedIn');
-    setIsLoggedIn(false);
+    setIsLogged(false);
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <AuthContext.Provider value={
+      {
+        isLoggedIn: isLogged
+      }
+    }>
+      <MainHeader onLogout={logoutHandler} />
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {!isLogged && <Login onLogin={loginHandler} />}
+        {isLogged && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
